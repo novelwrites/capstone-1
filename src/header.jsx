ebf8fa58ea9f90a34bcc94c83da3197e99975276
Nancy Golden <pngolden@verizon.net>
2020-06-08 23:24:11 -0500

    incorporated logo in header

--- src/header.jsx
+++ src/header.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function Header() {
   return (
     <div className="headerContainer">
-        <header><h1>This is header</h1></header>
+        {/*}<header><h1>This is header</h1></header>*/}
+   		
+        <img src='http://goldencrossranch.com/capstone1images/kiwilogo.jpg' alt="kiwi photagraphy logo" width="200" height="355"/> 
+        
+
     </div>
   );
 }
